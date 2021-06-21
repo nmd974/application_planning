9d0615e357a86f28203c1bcc9030b77d937823df
Gaetan LAVIT <g.lavit@outlook.fr>
2021-06-21 09:33:57 +0400

    Création des tables users et activity

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('promotion');
+            $table->date('birthday');
+            $table->enum('etat', ['actif', 'inactif']);
+            $table->foreignId('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
