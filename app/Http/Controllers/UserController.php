1392e2406ad43948be346c4f5298e56cf8907cc5
unknown <hassani.juniorsj@gmail.com>
2021-07-26 11:23:54 +0400

    Message de confirmation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,7 +54,7 @@ class UserController extends Controller
         User::create($request->all());
      
         return redirect()->route('users.index')
-                        ->with('success','L\'utilisateur à bien été crée.');
+                        ->with('messageSuccess','L\'utilisateur à bien été crée.');
     }
 
     /**
@@ -101,7 +101,7 @@ class UserController extends Controller
         $user->update($request->all());
     
         return redirect()->route('users.index')
-                        ->with('success','L\'utilisateur à bien été modifié');
+                        ->with('messageSuccess','L\'utilisateur à bien été modifié');
     }
 
     /**
@@ -115,5 +115,5 @@ class UserController extends Controller
         $user->delete();
     
         return redirect()->route('users.index')
-                        ->with('success','L\'utilisateur à bien été supprimé');
+                        ->with('messageSuccess','L\'utilisateur à bien été supprimé');
     }}
--- resources/views/includes/message-block.blade.php
+++ resources/views/includes/message-block.blade.php
@@ -1,8 +1,8 @@
 @if (Session::has('messageSuccess'))
-<div class="alert alert-success">{{ Session::get('messageSuccess') }}</div>
+<div class="alert alert-success"><center>{{ Session::get('messageSuccess') }}</center></div>
 {{Session::forget('messageSuccess')}}
 @endif
 @if (Session::has('messageError'))
-<div class="alert alert-danger">{{ Session::get('messageError') }}</div>
+<div class="alert alert-danger"><center>{{ Session::get('messageError') }}</center></div>
 {{Session::forget('messageError')}}
 @endif
